d3e627a64f203f76a5352749a9924c490d573c0a
Pedro Ciancaglini <pedrociancaglini@gmail.com>
2024-07-05 16:57:25 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,7 +24,7 @@ def load_data():
     docs = reader.load_data()
     
     llm = HuggingFaceInferenceAPI(
-        model_name= "mistralai/Mixtral-8x22B-v0.1", # "HuggingFaceH4/zephyr-7b-beta", #"mistralai/Mistral-7B-Instruct-v0.3",
+        model_name= "HuggingFaceH4/zephyr-7b-beta", #"mistralai/Mixtral-8x22B-v0.1", # "HuggingFaceH4/zephyr-7b-beta", #"mistralai/Mistral-7B-Instruct-v0.3",
         token=HF_KEY,
         max_new_tokens=256,
         temperature=0.7,
