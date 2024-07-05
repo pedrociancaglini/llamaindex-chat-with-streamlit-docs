d5c00ea41faa124215bf473c61c9a6b607e8ca7c
Pedro Ciancaglini <pedrociancaglini@gmail.com>
2024-07-05 17:22:52 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,21 +8,9 @@ st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaInd
 
 
 ### gif from url
-st.markdown("![Alt Text](https://github.com/pedrociancaglini/llamaindex-chat-with-streamlit-docs/blob/main/giphy.gif)")
+st.markdown("![Alt Text](giphy.gif)")
 st.image("giphy.gif")
 
-'''
-### gif from local file
-file_ = open("/giphy.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-    unsafe_allow_html=True,
-)
-'''
 
 HF_KEY = st.secrets["HF_KEY"]
 
